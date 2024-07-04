47724923adce9085098d5acf7aa02cb27310113d
MARI\mm040 <m.m.04240816@gmail.com>
2024-07-05 07:24:21 +0900

    categoryのAPIを作成

--- src/resources/js/App.vue
+++ src/resources/js/App.vue
@@ -1,12 +1,50 @@
-<script setup>
-import logo from "../images/icon2.jpg";
-</script>
-
 <template>
   <div>
     <div class="header">
-        <div class="logo__img"><img :src="logo"></div>
+      <div class="logo__img"></div>
       <h1>My dictionary</h1>
     </div>
+    <div class="word__add-box">
+      <BaseInput v-model:value="word" />
+      <BaseInput v-model:value="meaning" />
+      <BaseInput v-model:value="example_sentence" />
+      <BaseSelectButton />
+      <BaseButton @click="wordAdd(word)">登録</BaseButton>
+    </div>
+    {{ categoryLists }}
   </div>
 </template>
+
+<script>
+import axios from "axios";
+import BaseInput from "./components/BaseInput.vue";
+import BaseButton from "./components/BaseButton.vue";
+import BaseSelectButton from "./components/BaseSelectButton.vue";
+
+export default {
+  name: "app",
+  components: {
+    BaseInput,
+    BaseButton,
+    BaseSelectButton,
+  },
+  data() {
+    return {
+      word: "",
+      meaning: "",
+      example_sentence: "",
+      categoryLists: [],
+    };
+  },
+  methods: {
+    async getCategoryList() {
+      const resData = await axios.get("http://127.0.0.1/api/word/");
+      this.categoryLists = resData.data.data;
+    },
+    wordAdd(word) {},
+  },
+  created() {
+    this.getCategoryList();
+  },
+};
+</script>
---
+++ src/resources/js/components/BaseInput.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <input
+      class="form-input"
+      :value="value"
+      @input="$emit('update:value', $event.target.value)"
+    />
+  </div>
+</template>
+    
+<script>
+export default {
+  name:"BaseInput",
+  props: ["value"],
+  emits: ['update:value'],
+};
+</script>
+    
+<style scope>
+.form-input {
+  padding: 1rem;
+}
+</style>
